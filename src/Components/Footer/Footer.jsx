3d74ff88ddea60f30a7f14d4c6f83ab76fbd8721
Yash Mittal <mittalyas1234@gmail.com>
2024-02-22 18:55:41 +0530

    navbar and footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,79 @@
-import './Footer.css'
+import React from 'react';
+// import background from '../../assets/back.jpg';
+import './Footer.css';
+// import logo from '../../assets/ieee-logo.png';
+import logo from '../../assets/JSCOP_LOGO.png'
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { IoLocationSharp } from "react-icons/io5";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutubeSquare } from "react-icons/fa";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <>
-     <div>Footer</div> 
-    </>
-  )
-}
+    <div id="Contact">
+      <div className="footer">
+        <div className="Border">
+          {/* <img className="bg-img" src={background} alt="" /> */}
+          <div className="symbol">
+            <img className="logo" src={logo} alt="JSCOP Logo" />
 
-export default Footer
+          </div>
+          <a className='phone' href="https://www.google.com/maps/place/JAYPEE+INSTITUTE+OF+INFORMATION+TECHNOLOGY/@28.6312137,77.3683335,17z/data=!3m1!4b1!4m6!3m5!1s0x390ce551491b3ce7:0x7335d9fcfd4d9db0!8m2!3d28.631209!4d77.3709084!16zL20vMDlqcXJt?entry=ttu" target='_blank'>
+            <div className="Location">
+              <div>
+                <IoLocationSharp fontSize='medium' />
+                <div className='location-content'>
+                  <p>JSCOP 6.O</p>
+                  <p>Jaypee Institute Of Information Technology</p>
+                  <p> Sector-62 , Noida </p>
+                  <p>India</p>
+                </div>
+              </div>
+            </div>
+          </a>
+          <div className="contact">
+            <div>
+              <FaPhone />
+              <a className='phone' href="tel:+919116457772">Aman Tripathi:- +91 8115610105</a>
+
+            </div>
+            <div>
+              <FaPhone />
+              <a className='phone' href="tel:+919506424755">Harsh Sharma:- +91 8383936346</a>
+            </div>
+            <div>
+              <MdEmail />
+
+              <a href="mailto:mittalyas1234@gmail.com" className='phone' >Email - mittalyas1234@gmail.com</a>
+
+            </div>
+          </div>
+          <div className="find-us">
+            <div className="icons">Find Us Here</div>
+            <div className="social-icons">
+              <a href="https://www.linkedin.com/company/jiitopticachapter/" className="link">
+                <FaLinkedin />
+              </a>
+              <a href="https://www.facebook.com/jiitopticachapter/" className="link">
+                <FaSquareFacebook />
+              </a>
+              <a href="https://github.com/jiitopticachapter" className="link">
+                <FaGithubSquare />
+              </a>
+              <a href="https://www.instagram.com/jiitopticachapter/" className="link">
+                <FaInstagramSquare />
+              </a>
+              <a href="https://www.youtube.com/@jiitopticachapter7634" className="link">
+                <FaYoutubeSquare />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,34 @@
-import "./Navbar.css";
+import React, { useState } from 'react';
+import './Navbar.css';
+import navLogo from '../../assets/JSCOP_LOGO.png';
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [toggleIcon, setToggleIcon] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+    setToggleIcon(!toggleIcon);
+  };
+
   return (
     <>
-      <div>Header</div>
+      <div className={`NavbarComponent ${showMenu ? 'showMenu' : ''}`}>
+        <div className={`NavbarToggle ${toggleIcon ? 'active' : ''}`} onClick={toggleMenu}></div>
+        <div className="NavabrLogo">
+          <img src={navLogo} alt="" />
+        </div>
+        <div className={`NavbarContent ${showMenu ? 'showMenu' : ''}`}>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>TIMELINE</li>
+          <li>EVENTS</li>
+          <li>GALLERY</li>
+          <li>TEAM</li>
+          <li>CONTACT</li>
+        </div>
+      </div>
     </>
   );
 };
-
-export default Navbar;
+export default Navbar
