33c302713f47648476a08f9d15c2282c2ff1208b
Rajatt09 <rajatbhati9559@gmail.com>
2024-02-28 23:18:32 +0530

    added register page

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,113 @@
 import React from "react";
+import "./Register.css";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const Register = () => {
-  return <div>Register</div>;
-};
+function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    batch: "",
+    enroll: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    if (
+      !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(formData.email)
+    ) {
+      alert("Invalid email format");
+    }
+
+    if (!/^\d{10}$/.test(formData.phone)) {
+      alert("Invalid phone number");
+    }
+
+    try {
+      const res = await axios.post(`/register`, formData);
+      console.log(res.data);
+      if (res.data.success) {
+        toast.success("Registered successful");
+      } else {
+        toast.error("Invalid Credentials");
+        return;
+      }
+    } catch (error) {
+      console.error("Error during register:", error);
+      toast.error("An error occured during registration");
+    }
+  };
+
+  return (
+    <div className="main-register">
+      <div className="register-header">Register Now</div>
+      <div className="register-div">
+        <form onSubmit={handleSubmit}>
+          <div id="registerform">
+            <input
+              onChange={handleChange}
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              className="register-input"
+              required
+            />
+            <input
+              onChange={handleChange}
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="register-input"
+              required
+            />
+            <input
+              onChange={handleChange}
+              type="text"
+              name="phone"
+              placeholder="Phone Number"
+              className="register-input"
+              required
+            />
+            <input
+              onChange={handleChange}
+              type="text"
+              name="batch"
+              placeholder="Batch"
+              className="register-input"
+              required
+            />
+            <input
+              onChange={handleChange}
+              type="text"
+              name="enroll"
+              placeholder="Enrollment Number"
+              className="register-input"
+              required
+            />
+          </div>
+
+          <div className="register-btns">
+            <button className="register-btn" type="submit">
+              Button 1
+            </button>
+            <button className="register-btn">Button 2</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Register;
--- src/Pages/Event/Event.jsx
+++ src/Pages/Event/Event.jsx
@@ -1,29 +1,34 @@
-import { useNavigate } from 'react-router-dom';
-import './Event.css';
-import Detail from './EventDetail.jsx';
+import { useNavigate } from "react-router-dom";
+import "./Event.css";
+import Detail from "./EventDetail.jsx";
 
 const Event = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className='event-container'>
-      
-        {
-          Detail.map((item,index)=>(
-            <div key={index} onClick={()=>{navigate(`/${item.id}`)}} className='event-box'>
-              <span></span>
-              <div class="event-content">
-                <h2 className='event-heading'>{item.name}</h2>
-                 <img src={item.image} className='event-img'/> 
-                <p className='event-desc'>{item.description}</p>
-                <a href="#" className='event-btn'>Read More</a>
-              </div>
+      <div className="event-container">
+        {Detail.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              navigate(`/${item.id}`);
+            }}
+            className="event-box"
+          >
+            <span></span>
+            <div className="event-content">
+              <h2 className="event-heading">{item.name}</h2>
+              <img src={item.image} className="event-img" />
+              <p className="event-desc">{item.description}</p>
+              <a href="#" className="event-btn">
+                Read More
+              </a>
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Event
+export default Event;
