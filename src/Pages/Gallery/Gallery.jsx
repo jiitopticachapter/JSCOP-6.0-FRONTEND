d449df8b96d03fc5a38a207418c33c7b23097c0e
teesha17 <teeshu.2179@gmail.com>
2024-03-16 21:01:37 +0530

    updated gallery

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Gallery.css";
+import {} from '../../oldassets/galleryImages/4.jpg'
 export default function Gallery() {
   return (
     <div style={{"textAlign":"center"}}>
@@ -9,15 +10,15 @@ export default function Gallery() {
     <div className="gallery-container">
       
     <div class="gallery">
-  <img src="https://picsum.photos/id/1028/300/300" alt="a forest after an apocalypse"/>
-  <img src="https://picsum.photos/id/15/300/300" alt="a waterfall and many rocks"/>
-  <img src="https://picsum.photos/id/1040/300/300" alt="a house on a mountain"/>
-  <img src="https://picsum.photos/id/106/300/300" alt="sime pink flowers"/>
-  <img src="https://picsum.photos/id/136/300/300" alt="big rocks with some trees"/>
-  <img src="https://picsum.photos/id/1039/300/300" alt="a waterfall, a lot of tree and a great view from the sky"/>
-  <img src="https://picsum.photos/id/110/300/300" alt="a cool landscape"/>
-  <img src="https://picsum.photos/id/1047/300/300" alt="inside a town between two big buildings"/>
-  <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain"/>
+  <img src="src/oldassets/gallerygalleryImages/4.jpg" alt="a forest after an apocalypse"/>
+  <img src="src/oldassets/galleryImages/5.jpg" alt="a waterfall and many rocks"/>
+  <img src="src/oldassets/galleryImages/6.jpg" alt="a house on a mountain"/>
+  <img src="src/oldassets/galleryImages/7.jpg" alt="sime pink flowers"/>
+  <img src="src/oldassets/galleryImages/8.jpg" alt="big rocks with some trees"/>
+  <img src="src/oldassets/galleryImages/9.jpg" alt="a waterfall, a lot of tree and a great view from the sky"/>
+  <img src="src/oldassets/galleryImages/10.jpg" alt="a cool landscape"/>
+  <img src="src/oldassets/galleryImages/11.jpg" alt="inside a town between two big buildings"/>
+  <img src="src/oldassets/galleryImages/4.jpg" alt="a great view of the sea above the mountain"/>
 </div>
 </div>
 </div>
