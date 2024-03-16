3f40cce36fb436b25a23caa888278117fe284246
Rajatt09 <rajatbhati9559@gmail.com>
2024-03-16 19:04:36 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,40 @@ import Footer from "./Components/Footer/Footer.jsx";
 import EvetDetailPage from "./Pages/Event/EventDetailPage.jsx";
 
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Register from "./Components/Register/Register.jsx";
 import Layout from "./Pages/Layout/Layout.jsx";
 import Team from "./Pages/Team/Team.jsx";
+import Loader from "./Pages/Loader/Loader.jsx";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/:id" element={<EvetDetailPage />} />
-        <Route path='/team' element={<Team />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 12500);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  } else {
+    return (
+      <>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/:id" element={<EvetDetailPage />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/Loader/Loader.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import "./Loader.css"; // Import the CSS file
+import Stars from "../../Components/Stars/Stars";
+
+const Loader = () => {
+  const [starsData, setStarsData] = useState([]);
+  const [displaystar, setDisplayStar] = useState("display-none");
+
+  useEffect(() => {
+    // const space = document.querySelector(".space");
+    // space.classList.remove("display-none");
+    // setDisplayStar("");
+    // const initializeAnimation = () => {
+    //   // Generate stars data
+    //   const stars = [];
+    //   for (let i = 0; i < 70; i++) {
+    //     stars.push({
+    //       a: `${Math.random() * 260}vmax`,
+    //       b: `${Math.random() * 5200}deg`,
+    //       delay: `${Math.random() * 2.5 - 0.5}s`,
+    //     });
+    //   }
+    //   setStarsData(stars);
+
+    // Remove stars animation:
+    setTimeout(() => {
+      // if (space) {
+      //   space.classList.add("display-none");
+      setDisplayStar("");
+      // }
+    }, 4800); //7500
+    // };
+
+    // initializeAnimation();
+  }, []);
+
+  return (
+    <div className="loader-container">
+      <div className="space">
+        {/* {starsData.map((star, index) => (
+          <div key={index}>
+            <div
+              style={{
+                "--a": star.a,
+                "--b": star.b,
+                animationDelay: star.delay,
+              }}
+              className="data-star1"
+            ></div>
+            <div
+              style={{
+                "--a": star.a,
+                "--b": star.b,
+                animationDelay: star.delay,
+              }}
+              className="data-star2"
+            ></div>
+            <div
+              style={{
+                "--a": star.a,
+                "--b": star.b,
+                animationDelay: star.delay,
+              }}
+              className="data-star3"
+            ></div>
+          </div>
+        ))} */}
+        <div className={`${displaystar}`}>
+          <Stars />
+        </div>
+      </div>
+      <div className="mid-label">
+        <div className="mid-label__text">
+          JIIT OPTICA <br />
+          PRESENTS
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
