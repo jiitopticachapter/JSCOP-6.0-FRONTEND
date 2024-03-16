deb7408c39efa0622810661d4e148c68c0892529
Yash Mittal <mittalyas1234@gmail.com>
2024-03-16 19:12:09 +0530

    add css changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import EvetDetailPage from "./Pages/Event/EventDetailPage.jsx";
-
 import { Routes, Route } from "react-router-dom";
-
 import Register from "./Components/Register/Register.jsx";
 import Layout from "./Pages/Layout/Layout.jsx";
 // import Team from "./Pages/Team/Team.jsx";
-import Webteam from "./Pages/Webteam/Webteam.jsx";
+// import Webteam from "./Pages/Webteam/Webteam.jsx";
 import WebTeamComponent from "./Components/WebteamComponent/Webteamcomponent.jsx";
 
 
--- src/Pages/Webteam/Webteam.jsx
+++ src/Pages/Webteam/Webteam.jsx
@@ -4,20 +4,24 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './Webteam.css';
 const Webteam = ({ member }) => {
     return (
-        <div className="first hero" style={heroStyle}>
+        <div className="first hero web-member-card" style={heroStyle}>
+            {/* <div className='hero-description-bk' style={heroDescriptionBackgroundouter}> */}
             <div className="hero-description-bk" style={heroDescriptionBackground}></div>
+            {/* </div> */}
             <img src={member.photo} alt={member.name} style={photoStyle} />
             <div className="hero-description" style={heroDescriptionStyle}>
-                <h2>{member.name}</h2>
-                <h2>{member.surname}</h2>
-                <p>{member.designation}</p>
-                <div className="social-links">
-                    <a href={member.linkedin} >
-                        <i className="fab fa-linkedin"></i>
-                    </a>
-                    <a href={member.insta} >
-                        <i className="fab fa-instagram"></i>
-                    </a>
+                <div>
+                    <h2 style={{ textAlign: "center" }}>{member.name}</h2>
+                    <h2 style={{ textAlign: "center" }}>{member.surname}</h2>
+                    <p style={{ textAlign: "center" }}>{member.designation}</p>
+                    <div className="social-links webteam-icons">
+                        <a href={member.linkedin} >
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href={member.insta} >
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
 
@@ -35,11 +39,28 @@ const heroStyle = {
     overflow: 'hidden',
     boxShadow: '5px 5px 30px rgba(0, 0, 0, 0.3)',
     margin: '30px',
-    border: '2px solid white'
+    border: '2px solid  rgba(255, 255, 255, 0.9)'
 };
 
 const heroDescriptionBackground = {
-    backgroundImage: 'linear-gradient(0deg , #3f5efb, #fc466b)',
+    // backgroundImage: 'linear-gradient(0deg , #3f5efb, #fc466b)',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    backdropFilter: 'blur(4px)',
+    borderRadius: '30px',
+    position: 'absolute',
+    top: '75%',
+    left: '6px',
+    height: '75%',
+    width: '108%',
+    transform: 'skew(19deg, -15deg)'
+}
+
+const heroDescriptionBackgroundouter = {
+    // backgroundImage: 'linear-gradient(0deg , #3f5efb, #fc466b)',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    backdropFilter: 'blur(4px)',
+    border: '6px solid white',
+    padding: '6px',
     borderRadius: '30px',
     position: 'absolute',
     top: '75%',
