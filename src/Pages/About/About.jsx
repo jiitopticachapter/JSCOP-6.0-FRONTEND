01239d0b7f4349f478a72ce3917a57b8b500b70c
Rajatt09 <rajatbhati9559@gmail.com>
2024-03-17 19:05:26 +0530

    added about background

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,41 +4,42 @@ import { useEffect } from "react";
 
 const About = () => {
   return (
-    <>
-      <div className="about" id="aboutus">
-        <div className="left">
-          {/* <Stars /> */}
-          <div className="heading-about">WHAT OPTICA DOES ?</div>
+    <div className="about" id="aboutus">
+      <div className="left">
+        {/* <Stars /> */}
+        <div className="heading-about">WHAT OPTICA DOES ?</div>
+        <div className="home-gradient2"></div>
+        <div className="home-gradient2"></div>
+        <div className="home-gradient2"></div>
 
-          <div className="content">
-            <div>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
-              fuga quibusdam. Voluptates itaque maiores odio quaerat esse animi
-              corrupti consequatur asperiores voluptatum laborum. Aperiam magni
-              eligendi molestias, ipsam, soluta nam veritatis assumenda,
-              necessitatibus deleniti harum maiores modi nobis! Unde eaque nulla
-              amet cupiditate tempora, quaerat itaque officiis incidunt quis
-              possimus!
-            </div>
-            <div>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
-              labore sed quibusdam sequi tenetur vero at accusantium ratione
-              esse dolore magni voluptates alias, minus quod amet dolor expedita
-              aliquid? Rem, explicabo voluptatibus velit recusandae dolorum et
-              aut pariatur, natus, vero quidem laudantium quas illum dolore?
-              Maiores, beatae. Quos aliquid culpa dolor doloribus, maxime totam
-              accusantium commodi harum libero repellendus hic soluta. Ipsa
-              assumenda maxime perspiciatis sequi. Labore maiores vero fugiat
-              porro temporibus magnam quod, aliquid reiciendis accusamus nisi
-              beatae cumque cum dignissimos ducimus alias ipsum tenetur neque
-              fuga iusto, possimus eaque velit molestiae? Excepturi, doloremque
-              vel molestias sunt fugiat omnis?
-            </div>
+        <div className="content">
+          <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
+            fuga quibusdam. Voluptates itaque maiores odio quaerat esse animi
+            corrupti consequatur asperiores voluptatum laborum. Aperiam magni
+            eligendi molestias, ipsam, soluta nam veritatis assumenda,
+            necessitatibus deleniti harum maiores modi nobis! Unde eaque nulla
+            amet cupiditate tempora, quaerat itaque officiis incidunt quis
+            possimus!
+          </div>
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
+            labore sed quibusdam sequi tenetur vero at accusantium ratione esse
+            dolore magni voluptates alias, minus quod amet dolor expedita
+            aliquid? Rem, explicabo voluptatibus velit recusandae dolorum et aut
+            pariatur, natus, vero quidem laudantium quas illum dolore? Maiores,
+            beatae. Quos aliquid culpa dolor doloribus, maxime totam accusantium
+            commodi harum libero repellendus hic soluta. Ipsa assumenda maxime
+            perspiciatis sequi. Labore maiores vero fugiat porro temporibus
+            magnam quod, aliquid reiciendis accusamus nisi beatae cumque cum
+            dignissimos ducimus alias ipsum tenetur neque fuga iusto, possimus
+            eaque velit molestiae? Excepturi, doloremque vel molestias sunt
+            fugiat omnis?
           </div>
         </div>
-        {/* <div className="roc"></div> */}
       </div>
-    </>
+      {/* <div className="roc"></div> */}
+    </div>
   );
 };
 export default About;
