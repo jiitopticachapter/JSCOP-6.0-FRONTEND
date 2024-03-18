f5250ab28dc89abae52b84586272ae10da35f8d2
Rajatt09 <rajatbhati9559@gmail.com>
2024-03-19 02:40:58 +0530

    changed register page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import EvetDetailPage from "./Pages/Event/EventDetailPage.jsx";
 import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-import Register from "./Components/Register/Register.jsx";
 import Layout from "./Pages/Layout/Layout.jsx";
 // import Team from "./Pages/Team/Team.jsx";
 import Loader from "./Pages/Loader/Loader.jsx";
@@ -13,6 +12,7 @@ import Webteam from "./Pages/Webteam/Webteam.jsx";
 import WebTeamComponent from "./Components/WebteamComponent/Webteamcomponent.jsx";
 import ScrollToTop from "./Components/ScrollToTop/ScrollToTop.js";
 import About from "./Pages/About/About.jsx";
+import RegisterForm from "./Components/Register/RegisterForm.jsx";
 
 function App() {
   // return (
@@ -38,10 +38,10 @@ function App() {
     }, 10200);
   }, []);
 
-  if (loading) {
-    return <Loader />;
-  } else {
-    // if (1) {
+  // if (loading) {
+  //   return <Loader />;
+  // } else {
+  if (1) {
     return (
       <>
         <ScrollToTop />
@@ -50,7 +50,7 @@ function App() {
           <Route path="/" element={<Layout />} />
           <Route path="/:id" element={<EvetDetailPage />} />
           <Route path="/Webteam" element={<WebTeamComponent />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/register" element={<RegisterForm />} />
         </Routes>
         <Footer />
       </>
