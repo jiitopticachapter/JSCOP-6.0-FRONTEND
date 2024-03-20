385a2525cd8a5757e2dac177a463feaba2d15bf2
Rajatt09 <rajatbhati9559@gmail.com>
2024-03-21 00:35:06 +0530

    improved css

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -18,13 +18,9 @@ const Homepage = () => {
       <div className="home-heading">
         {/* <div className="home-heading-inner"> */}
         <div className="home-heading1">JIIT OPTICA</div>
-        <div className="home-break">
-          <br />
-        </div>
+        <div className="home-break">{/* <br /> */}</div>
         <div className="home-heading2">Presents</div>
-        <div className="home-break">
-          <br />
-        </div>
+        <div className="home-break">{/* <br /> */}</div>
         <div className="home-heading3">
           JSCOP <span className="six-point-zero"> 6.0</span>
         </div>
