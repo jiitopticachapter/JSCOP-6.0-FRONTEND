440281e5d5bb448744a35e55fbccfd14574eaeb8
Yash Mittal <mittalyas1234@gmail.com>
2024-04-16 14:09:58 +0530

    solve error in web team part

--- src/assets/Data/WebTeamdata.js
+++ src/assets/Data/WebTeamdata.js
@@ -3,7 +3,7 @@ const WebTeamData = [
     {
         name: "Harsh",
         surname: "Sharma",
-        photo: "src/assets/Webteamimages/Harsh_sharma.png",
+        photo: '/images/Webteamimages/Harsh_sharma.png',
         linkedin: "https://www.linkedin.com/in/harshsharma20503?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         insta: "https://www.instagram.com/x_harsh.sharma_x?igsh=c2YwZjFwYW42d2dp",
         designation: "Technical Head"
@@ -12,7 +12,7 @@ const WebTeamData = [
     {
         name: "Kaavya",
         surname: "Beniwal",
-        photo: "src/assets/Webteamimages/kaavyaberniwal.png",
+        photo: "/images/Webteamimages/kaavyaberniwal.png",
         insta: "https://www.instagram.com/kaavyabaranwal?igsh=aWlma2J2bHRpNTl3",
         linkedin: "https://www.linkedin.com/in/kaavya-baranwal?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         designation: "Technical Head"
@@ -20,7 +20,7 @@ const WebTeamData = [
     {
         name: "Teesha",
         surname: "Kakkar",
-        photo: "src/assets/Webteamimages/teesha.jpg",
+        photo: "/images/Webteamimages/teesha.jpg",
         insta: "https://www.instagram.com/tee_sha_14?igsh=NHY1a2RsMHQ2cGRu",
         linkedin: "https://www.linkedin.com/in/teesha-kakkar-7283b6264",
         designation: "Tech Volunteer"
@@ -29,7 +29,7 @@ const WebTeamData = [
 
         name: "Nandini",
         surname: "Jindal",
-        photo: "src/assets/Webteamimages/Nandini.jpeg",
+        photo: "/images/Webteamimages/Nandini.jpeg",
         linkedin: "https://www.linkedin.com/in/nandini-jindal-33a3a7282/",
         designation: "Tech Volunteer"
 
@@ -37,7 +37,7 @@ const WebTeamData = [
     {
         name: "Yash",
         surname: "Mittal",
-        photo: "src/assets/Webteamimages/YashMittal.jpeg",
+        photo: "/images/Webteamimages/YashMittal.jpeg",
         insta: "https://www.instagram.com/yashmittal_05?igsh=MXI2MGR4YWhwZnQ5Yw==",
         linkedin: "https://www.linkedin.com/in/yash-mittal-433307251/",
         designation: "Tech Volunteer"
@@ -46,7 +46,7 @@ const WebTeamData = [
     {
         name: "Kartik",
         surname: "Aggarwal",
-        photo: "src/assets/Webteamimages/kartik.jpg",
+        photo: "/images/Webteamimages/kartik.jpg",
         linkedin: "https://www.linkedin.com/in/kartik-aggarwal-951a6a194?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         insta: "https://www.instagram.com/hii__kartik?igsh=dGNncHFpaWdsdjc2",
         designation: "Tech Volunteer"
@@ -54,7 +54,7 @@ const WebTeamData = [
     {
         name: "Rajat",
         surname: "Bhati",
-        photo: "src/assets/Webteamimages/RajatBhati.jpg",
+        photo: "/images/Webteamimages/RajatBhati.jpg",
         linkedin: "https://www.linkedin.com/in/rajat-bhati-6a2114216/",
         insta: "https://www.instagram.com/rajat___.13?igsh=NWVpaWxxMzRpc2ht",
         designation: "Tech Volunteer"
@@ -64,7 +64,7 @@ const WebTeamData = [
     {
         name: "Rishika",
         surname: "Aggarwal",
-        photo: "src/assets/Webteamimages/Rishika.jpg",
+        photo: "/images/Webteamimages/Rishika.jpg",
         linkedin: "https://www.linkedin.com/in/rishika-aggarwal-462b2a280",
         insta: "https://www.instagram.com/rishika_772713/",
         designation: "Tech Volunteer"
@@ -73,7 +73,7 @@ const WebTeamData = [
     {
         name: "Aryan",
         surname: "Goyal",
-        photo: "/images/WebTeam/Aryan_goyal.jpg",
+        photo: "/images/Webteamimages/Aryan-hackathon.jpg",
         linkedin: "https://www.linkedin.com/in/aryan-goyal-0368b8247/",
         insta: "https://www.instagram.com/ag_0080/",
         designation: "Tech Volunteer"
@@ -83,7 +83,7 @@ const WebTeamData = [
     {
         name: "Kshitij",
         surname: "Gupta",
-        photo: "src/assets/Webteamimages/ksitij.jpeg",
+        photo: "/images/Webteamimages/ksitij.jpeg",
         linkedin: "https://www.linkedin.com/in/kshitijgupta0707/",
         insta: "https://www.instagram.com/iamkshitijm?igsh=MTZzenp0OGxoa3AzNg==",
         designation: "Tech Volunteer"
@@ -93,7 +93,7 @@ const WebTeamData = [
     {
         name: "Ayush",
         surname: "Tomar",
-        photo: "src/assets/Webteamimages/AyushTomar.jpg",
+        photo: "/images/Webteamimages/AyushTomar.jpg",
         linkedin: "https://www.linkedin.com/in/ayush-kumar-tomar-a1a368270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         insta: "https://www.instagram.com/tomar_ayush?igsh=NDYweTBwM3NnOGNw",
         designation: "Tech Volunteer"
