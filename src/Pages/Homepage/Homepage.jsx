bb9c564553c8debcb59ad29f2b43a2571db128f2
Harsh Sharma <harshsharma20503@gmail.com>
2024-04-21 22:12:15 +0530

    updated secretary error and conference on

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -9,61 +9,61 @@ import { useParams } from "react-router-dom";
 // import jscop6heading from "../../assets/jscop6heading2.png";
 
 const Homepage = () => {
-  const videoRef = useRef();
-  const setPlayBack = () => {
-    videoRef.current.playbackRate = 2;
-  };
-  const { id } = useParams();
+    const videoRef = useRef();
+    const setPlayBack = () => {
+        videoRef.current.playbackRate = 2;
+    };
+    const { id } = useParams();
 
-  useEffect(() => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [id]);
-  return (
-    <div id="home" className="homepage">
-      <Stars speed={0.04} />
+    useEffect(() => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [id]);
+    return (
+        <div id="home" className="homepage">
+            <Stars speed={0.04} />
 
-      <img src={planet} alt="planet" className="planet" />
-      <div className="home-heading">
-        {/* <div className="home-heading-inner"> */}
-        <div className="home-heading1">JIIT OPTICA</div>
-        <div className="home-break">{/* <br /> */}</div>
-        <div className="home-heading2">Presents</div>
-        <div className="home-break">{/* <br /> */}</div>
-        <div className="home-heading3">
-          {/* <img src={jscop6heading} alt="" className="homeLogo" />{" "} */}
-          JSCOP
-          <span className="six-point-zero"> 6.0</span>
-        </div>
-        <div className="home-heading-inner2">
-          JIIT STUDENTS CONFERENCE FOR OPTICS AND PHOTONICS
-        </div>
-        <div className="home-break">
-          <br />
-        </div>
-        <Timer />
-        <div className="home-break">
-          <br />
-        </div>
-        <div className="home-register-btn-div">
-          {/* <Link to="/register"> */}
-          <button className="home-register-btn">
-            Registerations Starting Soon
-          </button>
-          {/* </Link> */}
-        </div>
-        {/* </div> */}
-      </div>
-      <div className="astro"></div>
+            <img src={planet} alt="planet" className="planet" />
+            <div className="home-heading">
+                {/* <div className="home-heading-inner"> */}
+                <div className="home-heading1">JIIT OPTICA</div>
+                <div className="home-break">{/* <br /> */}</div>
+                <div className="home-heading2">Presents</div>
+                <div className="home-break">{/* <br /> */}</div>
+                <div className="home-heading3">
+                    {/* <img src={jscop6heading} alt="" className="homeLogo" />{" "} */}
+                    JSCOP
+                    <span className="six-point-zero"> 6.0</span>
+                </div>
+                <div className="home-heading-inner2">
+                    JIIT STUDENTS CONFERENCE ON OPTICS AND PHOTONICS
+                </div>
+                <div className="home-break">
+                    <br />
+                </div>
+                <Timer />
+                <div className="home-break">
+                    <br />
+                </div>
+                <div className="home-register-btn-div">
+                    {/* <Link to="/register"> */}
+                    <button className="home-register-btn">
+                        Registerations Starting Soon
+                    </button>
+                    {/* </Link> */}
+                </div>
+                {/* </div> */}
+            </div>
+            <div className="astro"></div>
 
-      <div className="spacestation"></div>
-      <div className="home-gradient"></div>
-      <div className="home-gradient"></div>
-      <div className="home-gradient"></div>
-    </div>
-  );
+            <div className="spacestation"></div>
+            <div className="home-gradient"></div>
+            <div className="home-gradient"></div>
+            <div className="home-gradient"></div>
+        </div>
+    );
 };
 
 export default Homepage;
