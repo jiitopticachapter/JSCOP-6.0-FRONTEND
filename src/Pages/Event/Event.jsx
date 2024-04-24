ce8cc3b7a944fca9bf6feea45d2077da04e556cd
Yash Mittal <mittalyas1234@gmail.com>
2024-04-24 21:57:53 +0530

    add changes and add event

--- src/Pages/Event/Event.jsx
+++ src/Pages/Event/Event.jsx
@@ -8,66 +8,35 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const Event = () => {
   const navigate = useNavigate();
-
   return (
     <div id="events">
       <div className="eventheading">EVENTS</div>
       <div className="event-container">
         {Detail.map((item, index) => (
-          (item.title === "CODE WARS" || item.title === "ELECTRIFY DASH") ? (
-            <div key={index} className="event-box">
-              <div className="event-cardss">
-                <div className="nft">
-                  <div className="mainn">
-                    <div style={{ justifyContent: "center", display: "flex" }}>
-                      <img className="tokenImage" src={item.imgsrc} style={{ height: "300px", width: "400px" }} alt="NFT" />
-                    </div>
-                    <h2 className="EventNameHEAD">{item.title}</h2>
-                    <div className="tokenInfo">
-                      <div className="duration">
-                        <Link
-                          to={`/eventDetails/${item.id}`}
-                          className="event-btn"
-                        >
-                          Read more
-                        </Link>
-                      </div>
-                    </div>
-                    <hr />
+
+          <div key={index} className="event-box">
+            <div className="event-cardss">
+              <div className="nft">
+                <div className="mainn">
+                  <div style={{ justifyContent: "center", display: "flex" }}>
+                    <img className="tokenImage" src={item.imgsrc} style={{ height: "300px", width: "400px" }} alt="NFT" />
                   </div>
+                  <h2 className="EventNameHEAD">{item.title}</h2>
+                  <div className="tokenInfo">
+                    <div className="duration">
+                      <Link
+                        to={`/eventDetails/${item.id}`}
+                        className="event-btn"
+                      >
+                        Read more
+                      </Link>
+                    </div>
+                  </div>
+                  <hr />
                 </div>
               </div>
             </div>
-          ) : <SkeletonTheme
-            key={index}
-            color="#e0e0e0"
-            baseColor="#cdcbcb"
-            highlightColor="#f5f5f5"
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <div style={{ padding: "0px 35px" }}>
-                <Skeleton width={226} height={200} />
-              </div>
-              <br />
-              <Skeleton width={180} height={25} />
-              <br />
-
-              <Skeleton width={100} height={28} />
-              <div style={{ color: "white" }}>
-                <br />
-                <h3>To Be Revealed Soon...</h3>
-              </div>
-            </div>
-            <br />
-            <br />
-          </SkeletonTheme>
+          </div>
         ))}
       </div>
     </div>
